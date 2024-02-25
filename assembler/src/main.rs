4e9f23d2ac890b9d67e5b6651a25f138d356d005
Olivia Webber <145234320+olivia-branch@users.noreply.github.com>
2024-02-24 19:16:56 -0800

    linting.

--- assembler/src/main.rs
+++ assembler/src/main.rs
@@ -1,8 +1,12 @@
-use oppsy::codes::MainInstructions;
 use deku::prelude::*;
+use oppsy::codes::MainInstructions;
 
 fn main() {
-    let prog = vec![MainInstructions::LDBC(5), MainInstructions::INCBC, MainInstructions::INCBC];
+    let prog = vec![
+        MainInstructions::LDBC(5),
+        MainInstructions::INCBC,
+        MainInstructions::INCBC,
+    ];
     let prog: Vec<_> = prog.iter().flat_map(|s| s.to_bytes().unwrap()).collect();
     dbg!(prog);
 }
