cfbd909abf06c0c400aa6db2c69827c8e6985324
Olivia Webber <145234320+olivia-branch@users.noreply.github.com>
2024-03-03 13:18:09 -0800

    ram increment working.

--- emulator/src/main.rs
+++ emulator/src/main.rs
@@ -1,17 +1,19 @@
 use oppsy::codes::MainInstructions;
 use std::fs;
 use std::env;
-use std::io::Read;
 use deku::prelude::*;
 use std::process::exit;
 
 #[derive(Debug)]
 struct State {
     a: u8,
+    h: u8,
+    l: u8,
 }
 
 struct Emulator {
     program: Vec<u8>,
+    stack: [u8; 65536],
     pc: usize,
     state: State,
 }
@@ -21,18 +23,37 @@ fn update_state(emulator: Emulator, instruction: MainInstructions) -> Emulator {
         MainInstructions::NOP => {
             Emulator {
                 program: emulator.program,
+                stack: emulator.stack,
                 pc: emulator.pc + 1,
                 state: emulator.state,
             }
         },
         MainInstructions::LDBC(_) => todo!(),
         MainInstructions::INCBC => todo!(),
-        MainInstructions::INCA => {
+        MainInstructions::LDHL(val) => {
+            let h = (val >> 8) as u8;
+            let l = val as u8;
             let state = State {
-                a: emulator.state.a + 1,
+                a: emulator.state.a,
+                h,
+                l,
             };
             Emulator {
                 program: emulator.program,
+                stack: emulator.stack,
+                pc: emulator.pc + 3,
+                state,
+            }
+        }
+        MainInstructions::INCA => {
+            let state = State {
+                a: emulator.state.a + 1,
+                h: emulator.state.h,
+                l: emulator.state.l,
+            };
+            Emulator {
+                program: emulator.program,
+                stack: emulator.stack,
                 pc: emulator.pc + 1,
                 state,
             }
@@ -40,13 +61,47 @@ fn update_state(emulator: Emulator, instruction: MainInstructions) -> Emulator {
         MainInstructions::LDA(val) => {
             let state = State {
                 a: val,
+                h: emulator.state.h,
+                l: emulator.state.l,
             };
             Emulator {
                 program: emulator.program,
+                stack: emulator.stack,
                 pc: emulator.pc + 2,
                 state,
             }
         },
+        MainInstructions::LDHLA => {
+            let mut stack = emulator.stack;
+            stack[emulator.get_hl_addr()] = emulator.state.a;
+            Emulator {
+                program: emulator.program,
+                stack: stack,
+                pc: emulator.pc + 1,
+                state: emulator.state,
+            }
+        }
+        MainInstructions::XORA => {
+            let state = State {
+                a: emulator.state.a ^ emulator.state.a,
+                h: emulator.state.h,
+                l: emulator.state.l,
+            };
+            Emulator {
+                program: emulator.program,
+                stack: emulator.stack,
+                pc: emulator.pc + 1,
+                state: state,
+            }
+        }
+        MainInstructions::JPN(val) => {
+            Emulator {
+                program: emulator.program,
+                stack: emulator.stack,
+                pc: val as usize,
+                state: emulator.state,
+            }
+        }
         MainInstructions::OUT(device) => {
             match device {
                 1 => println!("OUT: {}", emulator.state.a),
@@ -54,6 +109,7 @@ fn update_state(emulator: Emulator, instruction: MainInstructions) -> Emulator {
             }
             Emulator {
                 program: emulator.program,
+                stack: emulator.stack,
                 pc: emulator.pc + 2,
                 state: emulator.state,
             }
@@ -65,9 +121,12 @@ impl Emulator {
     pub fn new(program: Vec<u8>) -> Emulator {
         Emulator {
             program: program,
+            stack: [0_u8; 65536],
             pc: 0,
             state: State {
-                a: 0
+                a: 0,
+                h: 0,
+                l: 0,
             },
         }
     }
@@ -80,6 +139,10 @@ impl Emulator {
     fn get_state(&self) -> () {
         println!("PC: {}", self.pc);
         println!("STATE:\n {:?}", self.state);
+    }
+
+    fn get_hl_addr(&self) -> usize {
+        u16::from_le_bytes([self.state.l, self.state.h]) as usize
     }
 }
 
--- oppsy/src/codes.rs
+++ oppsy/src/codes.rs
@@ -9,10 +9,18 @@ pub enum MainInstructions {
     LDBC(u16),
     #[deku(id = "0x03")]
     INCBC,
+    #[deku(id = "0x21")]
+    LDHL(u16),
     #[deku(id = "0x3C")]
     INCA,
     #[deku(id = "0x3E")]
     LDA(u8),
+    #[deku(id = "0x77")]
+    LDHLA,
+    #[deku(id = "0xAF")]
+    XORA,
+    #[deku(id = "0xC3")]
+    JPN(u16),
     #[deku(id = "0xD3")]
     OUT(u8),
 }
